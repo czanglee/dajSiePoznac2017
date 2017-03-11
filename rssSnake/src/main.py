69fb10b12120d91264048cc545d9e361d87b77a7
czanglee <bogusp@gmail.com>
2017-03-11 13:50:25 +0100

    Hello world!

---
+++ rssSnake/src/main.py
@@ -0,0 +1,9 @@
+import datetime
+_author_ = 'czang'
+_project_ = 'rssSnake'
+
+print("Hello world!\nI'm czang :)\nnow is: "
+        +datetime.datetime.now()
+            .__str__()
+        +"\nand it's my first Python program"
+      )
