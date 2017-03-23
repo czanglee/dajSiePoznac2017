10514eeb923550ecf3b7f0d7a62eedda01132c73
czanglee <bogusp@gmail.com>
2017-03-23 21:13:12 +0100

    Classes

--- rssSnake/src/main.py
+++ rssSnake/src/main.py
@@ -1,17 +1,58 @@
 from urllib.request import urlopen
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as XmlTree
 
 _author_ = 'czang'
 _project_ = 'rssSnake'
 
-url = "https://czang.pl/blog/category/dsp2017/feed/"
-print('get data from: '+ url)
-req = urlopen(url)
-xmlData = req.read()
-xml = ET.fromstring(xmlData)
-for channel in xml.iter("channel"):
-    print("Channel: "+channel.find("title").text)
-    for item in channel.iter("item"):
-        print("Post: "+item.find("title").text
-                + " - " + item.find("pubDate").text
-              )
+
+class Rss:
+    channel = []
+
+    def __init__(self, url):
+        req = urlopen(url)
+        file = req.read()
+        xml = XmlTree.fromstring(file)
+        self.parse(xml)
+
+    def parse(self, xml):
+        for channel in xml.iter("channel"):
+            c = Channel(channel.find("title").text)
+            for item in channel.iter("item"):
+                c.posts.append(
+                    Post(item.find("title").text,
+                         item.find("pubDate").text
+                         )
+                )
+            self.channel.append(c)
+
+    def show(self):
+        for c in self.channel:
+            c.show()
+
+
+class Channel:
+    title = None
+    posts = []
+
+    def __init__(self, title):
+        self.title = title
+
+    def show(self):
+        print("Channel: "+self.title)
+        for p in self.posts:
+            p.show()
+
+
+class Post:
+    title = None
+    postDate = None
+
+    def __init__(self, title, postdate):
+        self.title = title
+        self.postDate = postdate
+
+    def show(self):
+        print("\t" + self.title + " - from: " + self.postDate)
+
+r = Rss("https://czang.pl/blog/category/dsp2017/feed/")
+r.show()
